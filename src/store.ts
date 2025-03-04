d2447669e84f91a5d92678dc95fb30091f78b2ea
Adam Nicholson <adamjnicholson1@gmail.com>
2025-03-04 16:31:28 +1000

    move env vars to .env file

--- src/store.ts
+++ src/store.ts
@@ -2,15 +2,27 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, signInAnonymously } from "firebase/auth";
+import { z } from "zod";
+
+const envVarSchema = z.object({
+  VITE_FIREBASE_API_KEY: z.string(),
+  VITE_FIREBASE_AUTH_DOMAIN: z.string(),
+  VITE_FIREBASE_PROJECT_ID: z.string(),
+  VITE_FIREBASE_STORAGE_BUCKET: z.string(),
+  VITE_FIREBASE_MESSAGING_SENDER_ID: z.string(),
+  VITE_FIREBASE_APP_ID: z.string(),
+});
+
+const envVars = envVarSchema.parse(import.meta.env);
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC3DfN9bWD4X74yJq2yvBuLUueLjNfMQ3E",
-  authDomain: "react-test-83908.firebaseapp.com",
-  projectId: "react-test-83908",
-  storageBucket: "react-test-83908.appspot.com",
-  messagingSenderId: "517042318646",
-  appId: "1:517042318646:web:b576b74e135719f860d797",
+  apiKey: envVars.VITE_FIREBASE_API_KEY,
+  authDomain: envVars.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: envVars.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: envVars.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: envVars.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: envVars.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
